bfcba44040cbb26a887d011d4808ee17fbd062f5
JC Olivares <juancri@juancri.com>
2021-11-15 03:39:18 -0600

    Moving all code to run.R Updating README.md

--- README.md
+++ README.md
@@ -1,20 +1,9 @@
-# 2017-playoff-probabilities
-### MLS Playoff probabilities for the 2017 season
+# FIFA World Cup Qatar 2022 - South America qualifiers probabilities
 
-The following code models MLS playoff probabilities, by simulating the games remaining in the season using the Dixon-Coles method for modeling scoring rates. This is just about the simplest thing we can implement. It does not account for player injuries or absences, upticks (or downticks) in recent form, or differences in play resulting from game states (this probably helps Atlanta), among many other things.
+This project is just a modification of [2017-playoff-probabilities](https://github.com/kcm30/2017-playoff-probabilities).
 
-**data_prep.R**
-First, run this script to prepare the `modeling_data.csv` file. It reads from `game_info.csv`, and transforms the typical Home-Away-Home Score-Away Score into a modeling file formatted for R's `glm` function. The idea is to model the rate at which a team will score goals in game, as a function of their offensive strength, their opponent's defense strength, and a home-field advantage indicator. So that is this modeling file arranges the data in that way.
+## Run
 
-**game_probs.R**
-Next, run this script to get the model's esimated win-loss-draw probabilities for the remaining games on the schedule.
-
-**sim_season.R**
-Lastly, this script uses the probabilities output above to simulate the games still remaining in the season, 10,000 times, to get estimated points and final positions. It then takes those 10,000 seasons and turns the results into probablity estimates. Currently the model does not account for tiebreakers, the first of which is total wins, then goal differential, then goals for.
-
-#### Further Reading
-Dixon and Coles' original paper -
-[Modeling Association Football Scores and Inefficiencies in the Football Betting Market](http://www.math.ku.dk/~rolf/teaching/thesis/DixonColes.pdf)
-
-538's method for predicting match outcomes -
-<https://fivethirtyeight.com/features/whats-new-in-our-2017-18-club-soccer-predictions/>
+- Edit files in [input](./input)
+- Run `Rscript run.R`
+- Check output files in the output directory
--- run.R
+++ run.R
@@ -1,3 +1,65 @@
+## do the 2017 data prep for playoff probs
+library(dplyr)
+
+###### Put the Modeling Data Together ##################
+results <- data.frame(read.csv('input/game_info.csv', stringsAsFactors = FALSE))
+ 
+## now split results so i can build models on it
+home.half <- results[, c('date', 'hteam', 'ateam', 'hfinal')]
+home.half <- setNames(home.half, c('date', 'team', 'opponent', 'goals'))
+home.half$home.adv <- 1
+home.half$date <- as.Date(home.half$date, "%m/%d/%Y")
+## set up away half of teams
+away.half <- results[, c('date', 'ateam', 'hteam', 'afinal')]
+away.half <- setNames(away.half, c('date', 'team', 'opponent', 'goals'))
+away.half$home.adv <- 0
+away.half$date <- as.Date(away.half$date, "%m/%d/%Y")
+final.results <- arrange(rbind.data.frame(home.half, away.half), date)
+write.csv(final.results, 'intermediate/modeling_data.csv')
+######################################################
+## Calculate the result probabilities for the remaining games on the schedule
+
+## read in the data needed
+schedule <- data.frame(read.csv('input/remaining_games.csv', stringsAsFactors = FALSE))
+modeling.data <- data.frame(read.csv('intermediate/modeling_data.csv', stringsAsFactors = FALSE))
+
+## build the model on all the modeling data
+model <- glm(goals ~ team + opponent + home.adv,
+                 family = poisson(link = log),
+                 data = modeling.data)
+
+## give the function a model, home team, away team, return result probabilities
+PredictGame <- function(model, hteam, ateam, rho = 0) {
+    hteam.lambda <- predict(model, data.frame(home.adv = 1, team = hteam, opponent = ateam), type = 'response')
+    ateam.lambda <- predict(model, data.frame(home.adv = 0, team = ateam, opponent = hteam), type = 'response')
+    hteam.dist <- dpois(0:8, lambda = hteam.lambda)
+    ateam.dist <- dpois(0:8, lambda = ateam.lambda)
+    score.matrix <- hteam.dist %o% ateam.dist
+    ## do the adjustments for independence
+    score.matrix[1,1] <- score.matrix[1,1]*(1-hteam.lambda*ateam.lambda*rho)
+    score.matrix[1,2] <- score.matrix[1,2]*(1+hteam.lambda*rho)
+    score.matrix[2,1] <- score.matrix[2,1]*(1+ateam.lambda*rho)
+    score.matrix[2,2] <- score.matrix[2,2]*(1-rho)
+    #####
+    draw.prob <- sum(diag(score.matrix))
+    away.prob <- sum(score.matrix[upper.tri(score.matrix)])
+    home.prob <- sum(score.matrix[lower.tri(score.matrix)])
+    return(c(home.prob, away.prob, draw.prob))
+}
+
+## get result probabilities for every game in the schedule
+result.probs <- c()
+for (i in seq(1, nrow(schedule))){
+    row <- schedule[i, ]
+    p <- PredictGame(model, unlist(row[1]), unlist(row[2]), rho = .01)
+    result.probs <- rbind(result.probs, p)
+    print(p)
+}
+
+rp <- setNames(data.frame(result.probs), c('hwin','awin','tie'))
+predictions <- cbind.data.frame(schedule, rp)[, c('Home', 'Away', 'hwin', 'awin', 'tie')]
+
+write.csv(predictions, 'output/predictions.csv')
 ## simulate the remaining games in the season
 library(dplyr)
 library(nnet)
