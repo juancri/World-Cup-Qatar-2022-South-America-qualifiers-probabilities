bea552946462dfd093311e32c4d1ec89d31519db
JC Olivares <juancri@juancri.com>
2021-11-16 14:05:58 -0600

    Fixing date format

--- run.R
+++ run.R
@@ -8,12 +8,12 @@ results <- data.frame(read.csv('input/game_info.csv', stringsAsFactors = FALSE))
 home.half <- results[, c('date', 'hteam', 'ateam', 'hfinal')]
 home.half <- setNames(home.half, c('date', 'team', 'opponent', 'goals'))
 home.half$home.adv <- 1
-home.half$date <- as.Date(home.half$date, "%m/%d/%Y")
+home.half$date <- as.Date(home.half$date, "%Y-%m-%d")
 ## set up away half of teams
 away.half <- results[, c('date', 'ateam', 'hteam', 'afinal')]
 away.half <- setNames(away.half, c('date', 'team', 'opponent', 'goals'))
 away.half$home.adv <- 0
-away.half$date <- as.Date(away.half$date, "%m/%d/%Y")
+away.half$date <- as.Date(away.half$date, "%Y-%m-%d")
 final.results <- arrange(rbind.data.frame(home.half, away.half), date)
 write.csv(final.results, 'intermediate/modeling_data.csv')
 ######################################################
